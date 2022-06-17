45d72c476a2b9ea19ac3eb4584f27ebc5135b5ab
ducaton <102594550+ducaton@users.noreply.github.com>
2022-06-17 16:54:10 +0500

    syslog и улучшение параллелизации сканирования

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ import asyncio
 from aiohttp import web
 from ipaddress import ip_address
 from json import dumps
+from syslog import syslog
 
 routes = web.RouteTableDef()
 
 async def portStatus(ip, port):
-  s = asyncio.open_connection(ip, port)
   try:
-    await asyncio.wait_for(s, timeout=1)
-    s.close()
-    return {"port": port, "state": "open"}
-  except asyncio.TimeoutError:
-    s.close()
+    s = asyncio.open_connection(ip, port)
+    try:
+      await asyncio.wait_for(s, timeout=1)
+      s.close()
+      return {"port": port, "state": "open"}
+    except asyncio.TimeoutError:
+      s.close()
+      return {"port": port, "state": "close"}
+  except:
     return {"port": port, "state": "close"}
 
-def portScan(ip, pFrom, pTo):
+async def portScan(ip, pFrom, pTo):
   tasks = []
   for port in range(pFrom, pTo):
     tasks.append(asyncio.create_task(portStatus(ip, port)))
@@ -25,45 +29,67 @@ def portScan(ip, pFrom, pTo):
 #GET /scan/<ip>/<begin_port>/<end_port>
 @routes.get("/scan/{tail:.*}")
 async def portScanner(request):
+  syslog(6, request.remote + " " + request.method + " " + request.path)
+
   link = request.path.split("/")
 
   if len(link) != 5:
-    return web.Response(text="Необходимый вид запроса: /scan/<ip>/<begin_port>/<end_port>", status=400)
+    err = "Необходимый вид запроса: /scan/<ip>/<begin_port>/<end_port>"
+    syslog(6, request.remote + " - некорректный запрос: " + err)
+    return web.Response(text=err, status=400)
   ip = link[2]
 
   try:
     pFrom = int(link[3])
     pTo = int(link[4])
   except ValueError:
-    return web.Response(text="Порты должны цифрами", status=400)
+    err = "Порты должны цифрами"
+    syslog(6, request.remote + " - некорректный запрос: " + err)
+    return web.Response(text=err, status=400)
 
   try:
     ip_address(ip)
   except:
-    return web.Response(text="Указан некорректный IP", status=400)
+    err = "Указан некорректный IP"
+    syslog(6, request.remote + " - некорректный запрос: " + err)
+    return web.Response(text=err, status=400)
 
   if not ((0 <= pFrom <= 65535) and (0 <= pTo <= 65535)):
-    return web.Response(text="Порты должны быть в диапазоне от 0 до 65535", status=400)
+    err = "Порты должны быть в диапазоне от 0 до 65535"
+    syslog(6, request.remote + " - некорректный запрос: " + err)
+    return web.Response(text=err, status=400)
 
   if pFrom > pTo:
-    return web.Response(text="Необходимо указывать порты от меньшего к большему", status=400)
-  
+    err = "Необходимо указывать порты от меньшего к большему"
+    syslog(6, request.remote + " - некорректный запрос: " + err)
+    return web.Response(text=err, status=400)
+
   scanResult = []
+  scanChunk = []
   # Больше значение = выше производительность, но слабые устройства могут не выдержать
+  # Ограничено операционной системой (ulimit -n)
   # Значения: 1-65535
-  scanChunk = 65535
+  scanChunkSize = 1000
 
-  if pTo - pFrom > scanChunk:
-    for reqPart in range(pFrom, pTo, scanChunk):
-      if pTo - reqPart < scanChunk:
-        scanResult += await portScan(ip, reqPart, pTo)
+  # Если диапазон превышает размер куска, то сканировать по частям
+  if pTo - pFrom > scanChunkSize:
+    for reqPart in range(pFrom, pTo, scanChunkSize):
+      # Если осталось сканировать меньше, чем ограничено куском, то его не использовать
+      if pTo - reqPart < scanChunkSize:
+        scanChunk = await portScan(ip, reqPart, pTo)
+        scanResult += await scanChunk
+      # Сканирование по размеру куска
       else:
-        scanResult += await portScan(ip, reqPart, reqPart+scanChunk)
+        scanChunk = await portScan(ip, reqPart, reqPart+scanChunkSize)
+        scanResult += await scanChunk
     # Чтобы range() не игнорировал последнее число
-    scanResult += await portScan(ip, pTo, pTo+1)
+    scanChunk = await portScan(ip, pTo, pTo+1)
+    scanResult += await scanChunk
   else:
-    scanResult = await portScan(ip, pFrom, pTo+1)
+    scanChunk = await portScan(ip, pFrom, pTo+1)
+    scanResult += await scanChunk
 
+  syslog(6, request.remote + " - диапазон просканирован и отправлен")
   return web.Response(text=dumps(scanResult), headers={"content-type": "application/json"})
 
 app = web.Application()
